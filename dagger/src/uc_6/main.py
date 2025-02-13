28b036bcde48d62884520dc904c8360671731816
Federico Fornari <federico.fornari@ecmwf.int>
2025-02-13 14:57:10 +0100

    Updated pipeline

--- dagger/src/uc_6/main.py
+++ dagger/src/uc_6/main.py
@@ -189,11 +189,15 @@ class Uc6:
     @function
     async def push(
         self,
+        bucket: Annotated[str, Doc("S3 Bucket")],
+        endpoint: Annotated[str, Doc("S3 Endpoint")],
+        access: Annotated[dagger.Secret, Doc("S3 Access Key")],
+        secret: Annotated[dagger.Secret, Doc("S3 Secret Key")],
         registry: Annotated[str, Doc("Registry address")],
         namespace: Annotated[str, Doc("Registry namespace")],
         repo: Annotated[str, Doc("Registry repo")],
-        app: Annotated[str, Doc("Application name")],
-        tag: Annotated[str, Doc("Image tag")],
+        srctag: Annotated[str, Doc("Source image tag")],
+        dsttag: Annotated[str, Doc("Destination image tag")],
         username: Annotated[str, Doc("Registry username")],
         password: Annotated[dagger.Secret, Doc("Registry password")],
         wkd: Annotated[
@@ -202,6 +206,8 @@ class Uc6:
         ],
     ) -> str:
         """Build and publish image from existing Dockerfile"""
+        accesskey = await access.plaintext()
+        secretkey = await secret.plaintext()
         blob = await self.encode(username, password)
         config = (
             dag.container()
@@ -219,18 +225,22 @@ class Uc6:
         )
         return await (
             dag.container(platform=dagger.Platform("linux/amd64"))
-            .from_("gcr.io/kaniko-project/executor:debug")
-            .with_file("/kaniko/.docker/config.json", config.file("/tmp/config.json"))
-            .with_directory("/kaniko/.docker", wkd)
+            .with_service_binding(
+                "registry.local",
+                self.registry(bucket, endpoint, accesskey, secretkey)
+            )
+            .from_("rapidfort/skopeo-ib:latest")
+            .with_file("/tmp/config.json", config.file("/tmp/config.json"), owner="1000:1000")
             .with_exec(
                 [
-                    "/kaniko/executor",
-                    "--context", 
-                    "dir:///kaniko/.docker/",
-                    "--dockerfile",
-                    "/kaniko/.docker/Dockerfile",
-                    "--destination",
-                    f"{registry}/{namespace}/{repo}/{app}:{tag}"
+                    "skopeo",
+                    "copy",
+                    "--src-tls-verify=false",
+                    "--src-no-creds",
+                    "--dest-authfile",
+                    "/tmp/config.json",
+                    f"docker://registry.local/{repo}:{srctag}",
+                    f"docker://{registry}/{namespace}/{repo}:{dsttag}"
                 ]
             )
             .stdout()
